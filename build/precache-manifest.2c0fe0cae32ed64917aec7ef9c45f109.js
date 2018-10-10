e0dcbbc1173da57fe8efda1f8c376ed975983e1a
spairo <nelixhost@gmail.com>
2018-10-09 19:12:06 -0500

    feat(app): base reactjs

---
+++ build/precache-manifest.2c0fe0cae32ed64917aec7ef9c45f109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c4cad89bc5a801a52245",
+    "url": "/static/js/main.c4cad89b.chunk.js"
+  },
+  {
+    "revision": "a2842c7a23b3814da855",
+    "url": "/static/js/1.a2842c7a.chunk.js"
+  },
+  {
+    "revision": "c4cad89bc5a801a52245",
+    "url": "/static/css/main.326f4be6.chunk.css"
+  },
+  {
+    "revision": "d9a093ddfe438822e496af2a35ba48f1",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import TopBar from './components/TopBar';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <TopBar></TopBar>
+    );
+  }
+}
+
+export default App;
